3b4e4d7914d2d2803f9420709329fee2d04a0e7e
neet-007 <moayedeffect@gmail.com>
2024-11-09 01:37:10 +0300

    i made a basic base component that my components can extend it holds state and a render function and a set state which are public and has private methods and functions to handle state change and render

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
-import { Elem as Elem1 } from "./lib/statePubSub.js"
+import { BaseComponent, Elem as Elem1 } from "./lib/statePubSub.js"
 
-const elem2 = new Elem1(1);
+function parent(child) {
+    document.body.appendChild(child);
+}
+
+class Test1 extends BaseComponent {
+    /**
+     * @constructor
+     * @param {Record<string, any>} props 
+     */
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+
+    render() {
+        console.log("rnder:", this.id);
+        const elem = document.createElement("div");
+        elem.id = this.id;
+
+        const div = document.createElement("div");
+        div.innerHTML = String(this.state.count);
+        elem.appendChild(div);
+
+        const button = document.createElement("button");
+        button.innerHTML = "incremnet";
+        button.onclick = () => this.setState(prev => ({ ...prev, count: this.state.count + 1 }));
+
+        elem.appendChild(button);
+
+        elem.appendChild(this.props.child);
+        return elem;
+    }
+}
+
+class Test2 extends BaseComponent {
+    /**
+     * @constructor
+     * @param {Record<string, any>} props 
+     */
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        console.log("rnder:", this.id);
+        const elem = document.createElement("div");
+        elem.id = this.id;
+        elem.innerHTML = "inner";
+
+        return elem
+    }
+}
+
+const test2 = new Test2({});
+const test1 = new Test1({ child: test2.render() });
+
+parent(test1.render());
+
